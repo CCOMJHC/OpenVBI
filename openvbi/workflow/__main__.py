072e2181bd75ed59eb599a325d5ec34c0996eeca
Brian Miles <selimnairb@gmail.com>
2025-03-22 12:13:25 +1300

    #8 Add support for rendering string fields and tracking their state

--- openvbi/workflow/__main__.py
+++ openvbi/workflow/__main__.py
@@ -1,32 +1,75 @@
+from abc import ABC
 from typing import cast
 import sys
 import tkinter as tk
 
-from openvbi.core.schema import open_schema, parse_schema, SchemaNode, SchemaObject
+from openvbi.core.schema import open_schema, parse_schema, SchemaNode, SchemaObject, SchemaRef, \
+    SchemaLeafString
+
+
+class SchemaNodeWidgetsRenderer(ABC):
+    pass
+
+class SchemaLeafStringRenderer(SchemaNodeWidgetsRenderer):
+    def __init__(self, parent_frame, name: str, leaf: SchemaLeafString, state: dict, row: int, column: int,
+                 *,
+                 pad_x: int = 10,
+                 pad_y: int = 5):
+        state[name] = tk.StringVar()
+        tk.Entry(parent_frame, textvariable=state[name]).grid(column=column, row=row)
+
+
+class SchemaObjectWidgetsRenderer(SchemaNodeWidgetsRenderer):
+    def __init__(self, parent_frame, name: str, obj: SchemaObject, state: dict,
+                 *,
+                 pad_x: int = 10,
+                 pad_y: int = 5):
+        self.state = {}
+        state[name] = self.state
+        self.properties: dict[str, SchemaNodeWidgetsRenderer] = {}
+
+        row = 0
+        for prop, value in obj.properties.items():
+            tk.Label(parent_frame, text=prop).grid(column=0, row=row)
+            if isinstance(value, SchemaLeafString):
+                self.properties[prop] = SchemaLeafStringRenderer(parent_frame, prop, value, self.state, column=1, row=row)
+            row += 1
+
+
+class SchemaRefWidgetsRender(SchemaNodeWidgetsRenderer):
+    def __init__(self, parent_frame, ref: SchemaRef, state: dict,
+                 *,
+                 pad_x: int = 10,
+                 pad_y: int = 5):
+        self.referents: dict[str, SchemaNodeWidgetsRenderer] = {}
+
+        self.frame = tk.LabelFrame(parent_frame, text=ref.name,
+                              padx=pad_x, pady=pad_y)
+        if ref.referent:
+            r = ref.referent
+            if isinstance(r, SchemaObject):
+                self.referents[r.name] = SchemaObjectWidgetsRenderer(self.frame, ref.name, r, state)
+
+        self.frame.pack(fill='x')
+
 
 class MainWindow:
     hor_pad = 10
     ver_pad = 5
 
     def __init__(self, root: tk.Tk, schema: SchemaObject) -> None:
-        self.sections: dict[str, tk.LabelFrame] = {}
+        self.properties: dict[str, SchemaNodeWidgetsRenderer] = {}
+        # Dict to hold state variables that will be used to generate a JSON document based on the schema
+        self.state = {}
 
         self.root = root
 
         self.main_frame = tk.Frame(root, padx=self.hor_pad, pady=self.ver_pad)
         self.main_frame.pack(fill='both')
 
-        for k, v in schema.properties.items():
-            frame = tk.LabelFrame(self.main_frame, text=k, padx=self.hor_pad, pady=self.ver_pad)
-            if hasattr(v, 'referent'):
-                r = v.referent
-                if isinstance(r, SchemaObject):
-                    row = 0
-                    for prop, value in r.properties.items():
-                        tk.Label(frame, text=prop).grid(column=0, row=row)
-                        row += 1
-            frame.pack(fill='x')
-            self.sections[v] = frame
+        for v in schema.properties.values():
+            if isinstance(v, SchemaRef):
+                self.properties[v.name] = SchemaRefWidgetsRender(self.main_frame, v, self.state)
 
         # Set up buttons for direct actions
         self.button_frame = tk.LabelFrame(self.main_frame, text='Actions', padx=self.hor_pad, pady=self.ver_pad)
@@ -58,7 +101,7 @@ def main():
     schema_node: SchemaNode = parse_schema(schema, None, None, None)
     schema_obj: SchemaObject = cast(SchemaObject, schema_node)
     schema_obj.resolve_refs()
-    print(f"Parsed schema AFTER resolving refs was:\n{schema_obj.to_string()}")
+    print(f"DEBUG: Parsed schema AFTER resolving refs was:\n{schema_obj.to_string()}")
 
     # root = Tk()
     # frm = ttk.Frame(root, padding=10)
@@ -74,9 +117,9 @@ def main():
     root = tk.Tk()
     root.title("OpenVBI Metadata Generator")
     main_window = MainWindow(root, schema_obj)
+    print(f"DEBUG: state: {main_window.state}")
     root.mainloop()
 
-    root.mainloop()
     sys.exit(0)
 
 if __name__ == '__main__':
